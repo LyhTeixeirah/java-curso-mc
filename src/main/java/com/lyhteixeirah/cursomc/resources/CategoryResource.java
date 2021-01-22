e1e7b4293d0410ec43eda383b06cabf5f502386b
Lielses Inocêncio Teixeira <lielses@live.com>
2021-01-22 07:48:19 -0300

    Testing First Domain Class - Category

--- src/main/java/com/lyhteixeirah/cursomc/resources/CategoryResource.java
+++ src/main/java/com/lyhteixeirah/cursomc/resources/CategoryResource.java
@@ -1,5 +1,10 @@
 package com.lyhteixeirah.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.lyhteixeirah.cursomc.entities.Category;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryResource {
     
     @GetMapping
-    public String listar() {
-        return "rest esta funcionando";
+    public List<Category> listar() {
+
+        Category category1 = new Category(1, "technology");
+        Category category2 = new Category(2, "Bureau");
+        
+        List<Category> list = new ArrayList<>();
+        list.add(category1);
+        list.add(category2);
+        
+        return list;
     }
 }
